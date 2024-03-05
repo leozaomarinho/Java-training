e2ca1142ab7aa857e39e898957d15c593f5bb1e1
Leonardo Marinho <leowalkg6@gmail.com>
2024-03-04 22:26:19 -0300

    fix design

--- estoque/src/exer_banco/Main.java
+++ estoque/src/exer_banco/Main.java
@@ -1,4 +1,5 @@
 package exer_banco;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,81 +8,64 @@ public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner input = new Scanner(System.in);
-		
-		//variaveis
+
+		// variáveis
 		int option;
-		double valorDep;
-		String numeroConta;
-		String nomeTitular;
-		Account conta;
-		
+		String numeroConta = "";
+		String nomeTitular = "";
+		Account conta = null;
+
 		System.out.println("-- Bem vindo ao JavaBank --");
+		// Se a opção não for sair, pedir informações do titular e criar a conta
+		System.out.println("Digite o nome do titular da conta: ");
+		nomeTitular = input.next();
+		int numConta = (int) (Math.random() * 1000);
+		numeroConta = String.valueOf(numConta);
+
 		do {
 			System.out.println("O que você deseja fazer? ");
-			System.out.println("1 - Realizar Deposito");
+			System.out.println("1 - Realizar Depósito");
 			System.out.println("2 - Realizar Saque");
 			System.out.println("3 - Ver saldo");
 			System.out.println("0 - Sair");
-			System.out.print("Digite uma opção valida: ");
+			System.out.print("Digite uma opção válida: ");
 			option = input.nextInt();
-			
-			
-			System.out.println("Digite o nome do titular da conta: ");
-			nomeTitular = input.next();
-			int numConta = (int) (Math.random() * 1000);
-			numeroConta= String.valueOf(numConta);
-			System.out.println("Caso nao deseje depositar nenhum valor, digite 0");
-			System.out.print("Digite o valor a ser depositado: ");
-			double valueDep = input.nextDouble();
-			if (valueDep ==0) {
-				conta =new Account(numeroConta,nomeTitular);
-				System.out.println("Conta criada com sucesso");
-				System.out.printf("Sr(a) "+nomeTitular+" ,o numero da sua conta é: "+numeroConta);
-			}
-			else {
-				conta = new Account(numeroConta,nomeTitular,valueDep);
-				System.out.println("Conta criada com sucesso");
-				System.out.printf("Sr(a) "+nomeTitular+" ,o numero da sua conta é: "+numeroConta+" e o saldo inicial é de: "+valueDep+"%.n");
-			}
-			if(option==0){
+
+			if (option != 0) {
+
+				if (option == 1) {
+					// Realizar depósito
+					System.out.println("Caso não deseje depositar nenhum valor, digite 0");
+					System.out.print("Digite o valor a ser depositado: ");
+					double valueDep = input.nextDouble();
+					conta = (valueDep == 0) ? new Account(numeroConta, nomeTitular)
+							: new Account(numeroConta, nomeTitular, valueDep);
+					System.out.println("Conta criada com sucesso");
+					System.out.printf("Sr(a) %s, o número da sua conta é: %s e o saldo inicial é de: %.2f%n",
+							nomeTitular, numeroConta, conta.getSaldo());
+				} else if (option == 2 || option == 3) {
+					// Verificar se a conta foi criada antes de realizar saque ou ver saldo
+					if (numeroConta.isEmpty()) {
+						System.out.println("A conta ainda não foi criada, não é possível realizar a operação.");
+					} else {
+						if (option == 2) {
+							// Realizar saque
+							System.out.println("Qual será o valor do saque? ");
+							double valueSaque = input.nextDouble();
+							conta.Saque(valueSaque);
+						} else if (option == 3) {
+							// Ver saldo
+							System.out.printf("O saldo da conta é: %.2f%n", conta.getSaldo());
+						}
+					}
+				}
+			} else {
 				System.out.println("Você escolheu sair.");
 				System.out.println("Encerrando o Java Bank...");
 			}
-			
-			else if(option==1) {
-				if (numeroConta.isEmpty()) {
-					System.out.println("A conta ainda não foi criada, não é possivel realizar o deposito.");
-				}
-				else{
-					System.out.println("Qual será o valor do deposito? ");
-					valorDep = input.nextDouble();			
-				}
-			}
-			else if(option==2){
-				System.out.println("Qual o valor a ser sacado? ");
-				double valueSaque = input.nextDouble();
-				double saldo = conta.getSaldo();
-				
-				if (valueSaque>saldo && saldo<=0){
-					System.out.print("O seu saldo ficara negativo, você deseja prosseguir? s/n ");
-					String opcao = input.next();
-					
-					if(opcao=="s") {
-						conta.Saque(valueSaque);
-					}
-				}
-				else {
-					conta.Saque(valueSaque);
-				}
-			}
-			else if(option==3){
-				conta.getSaldo();
-			}
-			
-			
-		}while(option != 0);
+
+		} while (option != 0);
 
 		input.close();
 	}
-
 }
