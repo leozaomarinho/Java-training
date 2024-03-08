75c042101e4860a85d7dddd4eb59c0aa0a85fd0b
Leonardo Marinho <leowalkg6@gmail.com>
2024-03-07 22:49:15 -0300

    build vector with object

---
+++ List_arrays_vectors/src/files/Product.java
@@ -0,0 +1,18 @@
+package files;
+
+public class Product {
+
+	public String NomeProd;
+	public double Valor;
+	public int Quant;
+	
+	public Product() {
+		
+	};
+	
+	public Product(String nome,double valor, int quant) {
+		this.NomeProd = nome;
+		this.Valor = valor;
+		this.Quant = quant;
+	}
+}
---
+++ List_arrays_vectors/src/files/VetorObjeto.java
@@ -0,0 +1,10 @@
+package files;
+
+public class VetorObjeto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
