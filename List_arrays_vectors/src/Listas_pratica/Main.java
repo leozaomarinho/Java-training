b8e0a7f1d27a3a4910ec85906c682a73e0f51b12
Leonardo Marinho <leowalkg6@gmail.com>
2024-03-17 23:27:14 -0300

    iniciando com listas

---
+++ List_arrays_vectors/src/Listas_pratica/Main.java
@@ -0,0 +1,15 @@
+package Listas_pratica;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+			
+		List[] lista = new List[];
+		
+		input.close();
+	}
+
+}
