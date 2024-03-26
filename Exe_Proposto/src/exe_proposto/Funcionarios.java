25284df835c9d976f54eadba9532aea106e4c8a3
Leonardo Marinho <leowalkg6@gmail.com>
2024-03-25 23:37:16 -0300

    building class employes

--- Exe_Proposto/src/exe_proposto/Funcionarios.java
+++ Exe_Proposto/src/exe_proposto/Funcionarios.java
@@ -2,11 +2,52 @@ package exe_proposto;
 
 public class Funcionarios {
 
-	public int id;
-	public String nome;
-	public double salario;
+	private int Id;
+	private String Nome;
+	private double Salario;
 	
-	public Funcionario() {
+	public Funcionarios() {
 		
+	};
+	
+	public Funcionarios(int id,String nome, double salario) {
+		this.Id=id;
+		this.Nome=nome;
+		this.Salario=salario;
 	}
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public double getSalario() {
+		return Salario;
+	}
+
+	public void setSalario(double salario) {
+		Salario = salario;
+	}
+
+	@Override
+	public String toString() {
+		return "Funcionarios [Id :" + Id + ", Nome :" + Nome + ", Salario :" + Salario + "]";
+	};
+	
+	
+	
+	
+	
+	
 }
