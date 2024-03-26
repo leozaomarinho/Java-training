2f14780f617d402640f97c4fa7e13744acadfa68
Leonardo Marinho <leowalkg6@gmail.com>
2024-03-26 00:00:33 -0300

    update

--- Exe_Proposto/src/exe_proposto/Main.java
+++ Exe_Proposto/src/exe_proposto/Main.java
@@ -4,12 +4,35 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String [] args) {
-		
+	public static void main(String[] args) {
+
 		Scanner input = new Scanner(System.in);
+
+		// variaveis
+		int quantFunc, id;
+		String nome;
+		double salario;
 		
 		
-		
+
+		System.out.println("Digite a quantidade de funcionarios que voce deseja cadastrar: ");
+		quantFunc = input.nextInt();
+
+		int[] funcionarios = new int[quantFunc];
+
+		for (int i = 0; i < quantFunc; i++) {
+			System.out.println("== Cadastro de funcionario ==");
+			id = i;
+			System.out.println("Digite o nome do funcionario: ");
+			nome = input.next();
+			System.out.println("Digite o salario do funcionario: ");
+			salario = input.nextDouble();
+			
+			Funcionarios funcionario = new Funcionarios(id,nome,salario);
+			
+			funcionarios[i] = funcionario;
+		}
+
 		input.close();
 	}
 }
