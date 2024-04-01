43186b3181b885d68ce5d710087b7b5ae3ba08a6
Leonardo Marinho <leowalkg6@gmail.com>
2024-03-31 21:43:57 -0300

    utilizando matrizes

---
+++ List_arrays_vectors/src/matriz/java.java
@@ -0,0 +1,28 @@
+package matriz;
+
+import java.util.Scanner;
+
+public class java {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int numLinhas;
+		int numCol;
+		
+		System.out.println("Digite a quantidade de linhas: ");
+		numLinhas = input.nextInt();
+		System.out.println("Digite a quantidade de colunas: ");
+		numCol = input.nextInt();
+		
+		for(int l =1;l<=numLinhas;l++) {
+			System.out.println();
+			System.out.println("Linha : "+l);
+			for(int c = 1;c<=numCol;c++) {
+				System.out.print(" Coluna : "+c);
+			}
+		}
+		
+	}
+
+}
