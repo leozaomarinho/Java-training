72f05beb1292b408f4c091c413944f34ef836cd4
Leonardo Marinho <leowalkg6@gmail.com>
2024-04-01 00:03:47 -0300

    working with vects

---
+++ List_arrays_vectors/src/matriz/Matriz.java
@@ -0,0 +1,18 @@
+package matriz;
+
+public class Matriz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int m=3,n=4;
+		int[][] mat = new int[m][n];
+		
+		for(int i =0;i<mat.length;i++) {
+			for(int j =0;j<mat.length;j++) {
+				
+			}
+		}
+	}
+
+}
