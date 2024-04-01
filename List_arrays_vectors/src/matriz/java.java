f579ef6aa0d6112a283f572c07e1bce3001167a7
Leonardo Marinho <leowalkg6@gmail.com>
2024-03-31 23:38:12 -0300

    fix matriz

--- List_arrays_vectors/src/matriz/java.java
+++ List_arrays_vectors/src/matriz/java.java
@@ -6,25 +6,25 @@ public class java {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
-		int numLinhas;
-		int numCol;
-		
-		System.out.println("Digite a quantidade de linhas: ");
-		numLinhas = input.nextInt();
-		System.out.println("Digite a quantidade de colunas: ");
-		numCol = input.nextInt();
-		int[][] mat = new int[numLinhas][numCol];
-		for(int l =1;l<=numLinhas;l++) {
-			System.out.println();
-			System.out.println("Linha : "+l);
-			for(int c = 1;c<=numCol;c++) {
-				mat[l][c] = c+1;
-				System.out.print(" Coluna : "+(c+1));
-			}
-		}
-		
-		input.close();
+
+        int numLinhas;
+        int numCol;
+
+        System.out.println("Digite a quantidade de linhas: ");
+        numLinhas = input.nextInt();
+        System.out.println("Digite a quantidade de colunas: ");
+        numCol = input.nextInt();
+        int[][] mat = new int[numLinhas][numCol];
+        for (int l = 0; l < numLinhas; l++) {
+            System.out.println();
+            System.out.println("Linha : " + (l + 1));
+            for (int c = 0; c < numCol; c++) {
+                mat[l][c] = c + 1;
+                System.out.print(" "+(c + 1));
+            }
+        }
+
+        input.close();
 	}
 
 }
