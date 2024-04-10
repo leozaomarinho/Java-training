4365e3a5dd4667ace1894db9c7e2126e79d6c324
Leonardo Marinho <leowalkg6@gmail.com>
2024-04-09 22:46:49 -0300

    fix

--- List_arrays_vectors/src/matriz/java.java
+++ List_arrays_vectors/src/matriz/java.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class java {
 
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
         int numLinhas;
         int numCol;
@@ -19,12 +19,12 @@ public class java {
             System.out.println();
             System.out.println("Linha : " + (l + 1));
             for (int c = 0; c < numCol; c++) {
-                mat[l][c] = c + 1;
-                System.out.print(" "+(c + 1));
+                mat[l][c] = l * numCol + c + 1;
+                System.out.print(" " + mat[l][c]);
             }
         }
 
         input.close();
-	}
+    }
 
 }
