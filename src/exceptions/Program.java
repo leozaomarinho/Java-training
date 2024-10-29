6190137942031fd29b965ef9fda3fa8d45dff59c
Leonardo Marinho <leowalkg6@gmail.com>
2024-10-28 22:48:04 -0300

    update code to best practices

--- src/exceptions/Program.java
+++ src/exceptions/Program.java
@@ -1,47 +1,50 @@
 package exceptions;
-
+import java.util.InputMismatchException;
 import java.util.Scanner;
-
 import entities.ContaBancaria;
 
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
 		
 		 Scanner input = new Scanner(System.in);
-		 int option;
-		 
+		 //variables
+		 int option =-1;	 
 		 ContaBancaria conta = new ContaBancaria();
 
-		 System.out.println("O que deseja fazer ?");
-		 System.out.println("1 - Sacar dinheiro");
-		 System.out.println("2 - Depositar dinheiro");
-		 System.out.println("3 - Ver Saldo");
-		 System.out.println("0 - Sair");
-		 
-		 option = input.nextInt();
 		 do {
+			 System.out.println("\nO que deseja fazer ?");
+			 System.out.println("1 - Sacar dinheiro");
+			 System.out.println("2 - Depositar dinheiro");
+			 System.out.println("3 - Ver Saldo");
+			 System.out.println("0 - Sair");
 		 try {
+			 System.out.println("Opção: ");
+			 option = input.nextInt();
+			 
 			 if(option == 1) {
-				 System.out.print("quanto voce deseja sacar? ");
+				 System.out.print("Quanto voce deseja sacar? ");
 				 double value = input.nextDouble();
 				 conta.sacar(value);
 			 }
-			 if(option ==2){
-				 System.out.print("quanto voce deseja depositar? ");
+			 else if(option ==2){
+				 System.out.print("Quanto voce deseja depositar? ");
 				 double value = input.nextDouble();
 				 conta.setSaldo(value);
 			 }
-			 if(option==3){
+			 else if(option==3){
 				 conta.getSaldo();
 			 }
-			if (option ==0){
+			 else if (option ==0){
 				 System.out.println("Voce escolheu sair!");
 				 break;
 			 }
 		 }
-		 catch(Exception e){
+		 catch(InputMismatchException e){
+			 System.out.println(" Entrada invalida. Por favor, insira um número.");
+			 input.next();
+		 }
+		 catch(Exception e) {
 			 System.out.println(e.getMessage());
 		 }
 		 finally {
